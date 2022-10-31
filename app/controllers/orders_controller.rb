8f63598f0fb28bbbc07ff583e04122e0dfc3fb15
Zsuzsanna Orehoczki <106694908+zynolia@users.noreply.github.com>
2022-10-31 19:39:12 +0000

    added orders_controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,21 @@
 class OrdersController < ApplicationController
+    def new
+    @order = Order.new
+  end
+
+  def create
+    @order.user = User.find(params[user_id])
+    @order = Order.create(order_params)
+    if @order.save
+      redirect_to order_path(@order), notice: "Order placed successfully."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:user_id, :yarn_id)
+  end
 end
