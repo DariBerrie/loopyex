a0a67e390f02289f582eeaaa7efd076e7fab55c5
Dari Goldman <darigoldman@gmail.com>
2022-11-01 19:20:54 +0100

    added routes for products controller actions

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :yarns
+  resources :products
   resources :orders, only: %i[ index show new create ]
   # Defines the root path route ("/")
   # root "articles#index"
