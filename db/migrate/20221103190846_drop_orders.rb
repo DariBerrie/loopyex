bd44ebd0f35160d5dbbd82107e4136b11344b7c0
Dari Goldman <darigoldman@gmail.com>
2022-11-03 20:13:39 +0100

    fix: trying to fix DB by removing yarns from schema

---
+++ db/migrate/20221103190846_drop_orders.rb
@@ -0,0 +1,5 @@
+class DropOrders < ActiveRecord::Migration[7.0]
+  def change
+    drop_table :orders
+  end
+end
---
+++ db/migrate/20221103191108_drop_yarns.rb
@@ -0,0 +1,5 @@
+class DropYarns < ActiveRecord::Migration[7.0]
+  def change
+    drop_table :yarns
+  end
+end
