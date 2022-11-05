24620275f1184af027ed8883ed0ba7b4b65bdae8
Dari Goldman <darigoldman@gmail.com>
2022-11-05 14:22:42 +0100

    fix: updated product model, products#show view and orders#new view

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_03_202628) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_05_112937) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -64,6 +64,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_03_202628) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "latitude"
+    t.float "longitude"
+    t.string "address"
     t.index ["user_id"], name: "index_products_on_user_id"
   end
 
