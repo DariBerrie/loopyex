503ff8c4478a9dd583fa73bc784f7a1166d38f37
Dari Goldman <darigoldman@gmail.com>
2022-11-05 12:57:27 +0100

    fix: nested orders routes in products, updated show view, created order view, and updated orders controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :products
-  resources :orders, only: %i[ index show new create ]
+  resources :products do
+    resources :orders, only: %i[ index show new create ]
+  end
   # Defines the root path route ("/")
   # root "articles#index"
 end
