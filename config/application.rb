f59eec13c2f9cd361e5c8d5e42ae977440e8b5f7
Dari Goldman <darigoldman@gmail.com>
2022-12-26 19:05:39 +0100

    fix: fie path in application.rb

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,7 @@ module Loopyex
     end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
-    config.autoload_paths += %W(#{config.root}/lib)
+    config.autoload_paths += %W(#{config.root}/app/services)
 
     # Configuration for the application, engines, and railties goes here.
     #
