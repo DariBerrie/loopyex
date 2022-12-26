258c4ca548cdb887381ebf69bb61551150123adc
Dari Goldman <darigoldman@gmail.com>
2022-12-26 22:18:16 +0100

    fix

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -9,7 +9,7 @@ Stripe.api_key = Rails.configuration.stripe[:secret_key]
 StripeEvent.signing_secret = Rails.configuration.stripe[:signing_secret]
 
 StripeEvent.configure do |events|
-  events.subscribe 'checkout.session.completed', StripeCheckoutSessionService.new
+  events.subscribe('checkout.session.completed', StripeCheckoutSessionService.new)
 end
 
 private
