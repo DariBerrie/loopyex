1d42a209ca2fc8735685ec39adb4af0f436ffce6
Dari Goldman <darigoldman@gmail.com>
2022-12-26 18:51:38 +0100

    fix: changed co session service name

--- app/services/stripe_checkout_session_service.rb
+++ app/services/stripe_checkout_session_service.rb
@@ -1,6 +1,6 @@
-class StripeCheckoutSessionService
+class CheckoutSessionService
   def call(event)
-    order = Order.find_by(checkout_session_id: event.data.object.id)
+    order = Order.find(checkout_session_id: event.data.object.id)
     order.update(state: 'paid')
   end
 end
