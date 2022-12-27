8277ba960391aefd7efcd8892ebd077d0dd1390f
Dari Goldman <darigoldman@gmail.com>
2022-12-27 09:42:45 +0100

    fix

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,6 @@ module Loopyex
     end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
-    Spring.watch "app/services/**"
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
