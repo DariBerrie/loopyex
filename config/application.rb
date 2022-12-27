191cb03c8e6049fc295703bf79a0d02be2d771b5
Dari Goldman <darigoldman@gmail.com>
2022-12-27 09:41:54 +0100

    fix

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,7 @@ module Loopyex
     end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+    Spring.watch "app/services/**"
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -8,5 +8,5 @@ Stripe.api_key = Rails.configuration.stripe[:secret_key]
 StripeEvent.signing_secret = Rails.configuration.stripe[:signing_secret]
 
 StripeEvent.configure do |events|
-  events.subscribe('checkout.session.completed', StripeCheckoutSessionService.new)
+  events.subscribe 'checkout.session.completed', StripeCheckoutSessionService.new
 end
